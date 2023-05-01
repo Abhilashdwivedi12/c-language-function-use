3c546963e7c671d9d48495abdd7760653cd951b7
Abhilash Dwivedi <107290844+Abhilashdwivedi12@users.noreply.github.com>
2023-05-02 00:54:04 +0530

    Add files via upload
    multiplication of matrix and addition of matrix is here

---
+++ addition of matrix.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][2],b[2][2],c[2][2],i,j,k,sum=0;
+
+	printf("enter the first matrix");
+	for(int i=0;i<2;i++)
+	{
+		for(int j=0;j<2;j++)
+	
+	
+	scanf("%d",&a[i][j]);
+	{
+	
+	printf("\n");
+	}}
+		printf("enter the second matrix");
+	for(int i=0;i<2;i++)
+	{
+		for(int j=0;j<2;j++)
+	
+	
+	scanf("%d",&b[i][j]);
+	{
+	printf("\n");
+}}
+	
+	for(int i=0;i<2;i++)
+	{
+		for(int j=0;j<2;j++)
+	{
+		sum=0;
+		for(int k=0;k<2;k++){
+			sum=sum+a[i][j]*b[i][j];
+		}
+	}
+	
+	
+}
+	{
+	printf("\n");
+}
+		printf("the multiplication\n");
+	for(int i=0;i<2;i++)
+	{
+		for(int j=0;j<2;j++)
+	
+	
+	printf("%d",sum);
+	{
+	printf("\n");
+}
+}
+}
+	
+	
+
