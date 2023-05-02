90ee0934960f3f58db34093d9226e8376e95465e
Abhilash Dwivedi <107290844+Abhilashdwivedi12@users.noreply.github.com>
2023-05-03 00:06:29 +0530

    Add files via upload

---
+++ tic tac game.cpp
@@ -0,0 +1,156 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	printf("\t\t\t\t\t Welcome in my game world\n");
+	printf("\t\t\t\t\t TIC TAC GAME\n\n\n");
+
+
+	printf("\n");
+	
+
+	printf("\n");
+	printf("\t\t\t\t\t_1_|_2_|_3_\n\t\t\t\t\t_4_|_5_|_6_\n\t\t\t\t\t 7 | 8 | 9 ");
+	printf("\n\n\n");
+	
+	char tictac[4][4] ;{
+	
+	
+	for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		
+		printf("             | ");
+	}
+printf("\n");
+for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		
+		printf("             | ");
+	} 
+printf("\n");
+	for(int i=1;i<2;i++)
+	{
+		for(int j=0;j<=20;j++)
+		printf(" _");
+	}
+	printf("\n");
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		
+		printf("             | ");
+	} 
+printf("\n");
+for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		
+		printf("             | ");
+	} 
+printf("\n");
+	for(int i=1;i<2;i++)
+	{
+		for(int j=0;j<=20;j++)
+		printf(" _");
+	}
+	printf("\n");
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		
+		printf("             | ");
+	} 
+printf("\n");
+for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<=1;j++)
+		
+		printf("             | ");
+	} 
+printf("\n");
+
+	printf("\n");
+	for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("1_");
+		
+	}	for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("|2");
+		
+	}
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("_");
+		
+	}
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("|3_");
+		{
+			printf("\n");
+		}
+	}
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("4_");
+		
+	}	for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("|5");
+		
+	}
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("_");
+		
+	}
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("|6_");
+		{
+			printf("\n");
+		}
+	}
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("7_");
+		
+	}	for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("|8");
+		
+	}
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("_");
+		
+	}
+		for(int i=0;i<1;i++)
+	{
+		for(int j=0;j<1;j++)
+		printf("|9_");
+		{
+			printf("\n");
+		}
+	}
+	
+	
+	return 0;
+	
+}}
+
