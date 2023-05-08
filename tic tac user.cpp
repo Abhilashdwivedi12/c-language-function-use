f5af6e27a87a45734e12fdb8a8ad15518baf86e0
Abhilash Dwivedi <107290844+Abhilashdwivedi12@users.noreply.github.com>
2023-05-09 00:20:42 +0530

    Add files via upload

---
+++ tic tac user.cpp
@@ -0,0 +1,179 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	printf("\t\t\t\t\t Welcome in my game world\n");
+	printf("\t\t\t\t\t TIC TAC GAME\n\n\n");
+
+
+	printf("\n");
+	
+	char index[]={'1','2','3','4','5','6','7','8','9'};
+	int m;
+	int n;
+//	char X;
+
+
+{
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	
+	printf("mark your move ");
+	scanf("%d",&m);
+	index[m-1]='X';
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+		printf("mark your move ");
+	scanf("%d",&n);
+	index[n-1]='o';
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("mark your move ");
+	scanf("%d",&m);
+	index[m-1]='X';
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+		printf("mark your move ");
+	scanf("%d",&n);
+	index[n-1]='o';
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("mark your move ");
+	scanf("%d",&m);
+	index[m-1]='X';
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+		printf("mark your move ");
+	scanf("%d",&n);
+	index[n-1]='o';
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("mark your move ");
+	scanf("%d",&m);
+	index[m-1]='X';
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+		printf("mark your move ");
+	scanf("%d",&n);
+	index[n-1]='o';
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("mark your move ");
+	scanf("%d",&m);
+	index[m-1]='X';
+	
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[0],index[1],index[2]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[3],index[4],index[5]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("_____________________________________________________\n");
+	printf("\t   \t |\t   \t |\t   \t \n");
+	printf("\t %c \t |\t %c \t |\t %c \t \n",index[6],index[7],index[8]);
+	printf("\t   \t |\t   \t |\t   \t \n");
+	if(1=X&&2==X&&3==X||1=O&&2==O&&3==O){
+		printf("you are winner");
+	}
+	else if(4=X&&5==X&&6==X||4=O&&5==O&&6==O){
+		printf("you are winner");
+	}
+		else if(7=X&&8==X&&4==X||7=O&&5==O&&6==O){
+		printf("you are winner");
+	}
+	
+}}
+
+
